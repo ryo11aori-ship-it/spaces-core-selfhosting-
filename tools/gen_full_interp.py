ad8c6d9249866f79bbca2c9c4bee9a5a9fe55141
ryo11aori-ship-it <ryo11aori@gmail.com>
2025-12-29 23:08:38 +0900

    gen_full_interp.py を更新

--- tools/gen_full_interp.py
+++ tools/gen_full_interp.py
@@ -8,106 +8,107 @@ def main():
     def l(c): return "[" + c + "]"
     def clr(): return "[-]"
 
-    # --- Full Interpreter Logic ---
-    # Memory Layout: 0 0 [StartMarker=1] [Code...] 0 [Temp/Op] [Flag] 0 [Data...]
+    # --- Robust Depth-1 Interpreter Logic ---
+    # Memory Layout: [Code...] 0 [Temp] [Flag] 0 [Data...]
     
-    # Macro: Move Focus
-    to_data = "[<]>[>]>"
-    to_code = "<[<]>[>]<" 
+    # Macros for movement
+    # to_data: Assumes we are at an Opcode (non-zero). Moves right past 0 to Data.
+    to_data = "[>]>>" 
+    # to_code: Assumes we are at Data (non-zero or zero). Moves left past 0 to Opcode.
+    # Note: Data might be 0, so we rely on the separator structure.
+    # Structure: 0 [Flag] [Temp] 0 [CodeEnd]
+    # We explicitly move left fixed steps from Data.
+    to_code = "<<[<]" 
 
     bf = ""
-    # 1. Skip Header
+    # 1. Skip Header (S, P, A)
     bf += m(1) + ","*3 
     
     # 2. Read Code (until 0)
+    # Layout: 0 [Code...] 0
     bf += m(3) + a(1) + l( "," + l( m(1) ) + a(1) + m(-1) + s(1) + m(-1) ) + m(2)
     
-    # 3. Go to Code Start
-    bf += l( l( m(1) ) + a(1) + m(-1) + s(1) + m(-1) ) + m(1)
+    # 3. Execution Loop
+    # Go to Code Start
+    bf += "<[<]>"
     
-    # 4. Main Loop
+    # Main Loop (Iterate over Code)
     bf += "["
     
-    # Copy Opcode to Temp
+    # Copy Opcode to Temp for checking
+    # Layout: [Op] [Temp]
     bf += l( m(1)+a(1)+m(1)+a(1)+m(-2) ) + m(2) + l(m(-2)+a(1)+m(2)) + m(-2)
     
-    # --- Action Definitions (Defined separately to avoid SyntaxError) ---
-    
-    # Action 0x08 (]): Scan Backward if Data != 0
-    # Scan logic: Counter starts at 1. Move Left. If ] inc, if [ dec. Stop at 0.
-    # Note: 0x07=[, 0x08=]
-    scan_back = (
-        to_code + m(-1) + l(m(-1)) + m(-1) # Move Left past current op
-        + l( # Scan Loop
-             m(-1) + l(m(-1)) + m(1) # Move to Op
-             + s(7) + l( # Not 0x07 ([)
-                 s(1) + l( # Not 0x08 (])
-                     a(8) # Restore
-                     + clr()
-                 )
-                 + a(1) + l( # Is 0x08 (]) -> Nested
-                     a(7) + m(1) + a(1) + m(-1) # Inc Counter
-                     + clr()
-                 )
-                 + clr()
-             )
-             + a(1) + l( # Is 0x07 ([) -> Matching or Nested
-                 a(7) + m(1) + s(1) + m(-1) # Dec Counter
-                 + clr()
-             )
-             + a(7) + m(1) # Check Counter
-        )
-        + m(2) + clr() + m(1) # Clear Flag, Return to Data
-    )
-    act_8 = ">" + l( to_data + l( scan_back ) + to_code + clr() ) + "<"
-
-    # Action 0x07 ([): Scan Forward if Data == 0
-    # Scan logic: Counter starts at 1. Move Right. If [ inc, if ] dec. Stop at 0.
+    # --- SCAN LOGIC (Simplified for Stability) ---
+    # scan_fwd: Search for ] (0x08). 
+    # Logic: Move Right. Sub 8. If 0, Stop. Else Add 8, Repeat.
+    # At end: Add 8 to restore the ] we found.
     scan_fwd = (
-        to_code + m(1) + l(m(1)) + m(1) # Move Right past current op
-        + l( # Scan Loop
-             m(1) + l(m(1)) + m(-1) # Move to Op
-             + s(7) + l( # Not 0x07 ([)
-                 s(1) + l( # Not 0x08 (])
-                     a(8) + clr()
-                 )
-                 + a(1) + l( # Is 0x08 (]) -> Matching or Nested
-                     a(7) + m(-1) + s(1) + m(1) # Dec Counter
-                     + clr()
-                 )
-                 + clr()
-             )
-             + a(1) + l( # Is 0x07 ([) -> Nested
-                 a(7) + m(-1) + a(1) + m(1) # Inc Counter
-                 + clr()
-             )
-             + a(7) + m(-1) # Check Counter
-        )
-        + m(2) + clr() # Clear Flag
+        ">" + s(8) + l( # Move Next, Check ]
+            a(8) + ">" + s(8) # Restore, Move Next, Check ]
+        ) + a(8) # Restore the found ]
     )
-    act_7 = ">" + l( 
-        to_data + l( to_code + clr() ) # Data!=0, Enter loop (Clear Flag)
-        + a(1) + l( # Data==0, Scan Forward
-             scan_fwd
-        ) + clr()
+
+    # scan_back: Search for [ (0x07).
+    # Logic: Move Left. Sub 7. If 0, Stop. Else Add 7, Repeat.
+    scan_back = (
+        "<" + s(7) + l( # Move Prev, Check [
+            a(7) + "<" + s(7) # Restore, Move Prev, Check [
+        ) + a(7) # Restore the found [
+    )
+
+    # --- DECODE ---
+    # Opcode checks 1..8
+    
+    # 0x01 (>)
+    act_1 = ">" + l( to_data + ">" + to_code + clr() ) + "<"
+    # 0x02 (<)
+    act_2 = ">" + l( to_data + "<" + to_code + clr() ) + "<"
+    # 0x03 (+)
+    act_3 = ">" + l( to_data + a(1) + to_code + clr() ) + "<"
+    # 0x04 (-)
+    act_4 = ">" + l( to_data + s(1) + to_code + clr() ) + "<"
+    # 0x05 (.)
+    act_5 = ">" + l( to_data + "." + to_code + clr() ) + "<"
+    # 0x06 (,)
+    act_6 = ">" + l( to_data + "," + to_code + clr() ) + "<"
+    
+    # 0x07 ([)
+    # If Data==0, Scan Fwd. Else Enter (do nothing).
+    act_7 = ">" + l(
+        to_data + l( # Data != 0
+             to_code + clr() # Exit Action (Clear Flag)
+        )
+        + a(1) + l( # Data == 0 (Flag is 1)
+             to_code + scan_fwd # Go back to code and Scan!
+             + clr() # Clear Flag
+        )
+        + clr()
     ) + "<"
 
-    act_6 = ">" + l( to_data + "," + to_code + clr() ) + "<"
-    act_5 = ">" + l( to_data + "." + to_code + clr() ) + "<"
-    act_4 = ">" + l( to_data + s(1) + to_code + clr() ) + "<"
-    act_3 = ">" + l( to_data + a(1) + to_code + clr() ) + "<"
-    act_2 = ">" + l( to_data + "<" + to_code + clr() ) + "<"
-    act_1 = ">" + l( to_data + ">" + to_code + clr() ) + "<"
+    # 0x08 (])
+    # If Data!=0, Scan Back. Else Exit (do nothing).
+    act_8 = ">" + l(
+        to_data + l( # Data != 0
+             to_code + scan_back # Go back to code and Scan!
+             + m(1) + clr() + m(-1) # Hack: Clear Flag (Temp is at m(1) relative to code?)
+             # Wait, to_code brings us to [Op]. Temp is >. Flag is >>.
+             # We need to clear Flag (Temp).
+        )
+        + to_code + clr() # Clear Flag (Data==0 case or after scan)
+    ) + "<"
+    # Note: to_data/to_code macros align us to [Op]. 
+    # The 'l' loop surrounds the Flag check. 'clr()' clears the Flag/Temp.
 
-    # --- Build Decode Tree ---
+    # Build Tree
     bf += s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( ">a<" + s(1) + l( 
         clr() 
     ) + act_8 ) + act_7 ) + act_6 ) + act_5 ) + act_4 ) + act_3 ) + act_2 ) + act_1
     
-    # Next Instruction
-    bf += ">>]"
+    # Move to Next Instruction
+    bf += ">]"
 
-    # Convert to Spaces
+    # Convert
     S, F = " ", "\u3000"
     mapping = {'>':S*3, '<':S*2+F, '+':S+F+S, '-':S+F+F, '.':F+S+S, ',':F+S+F, '[':F*2+S, ']':F*3}
     res = "".join([mapping[c] for c in bf if c in mapping])
